fa565cd64b86e5509f8cb10bab5d88a7b18c2385
Jimmy <g08163314@gmail.com>
2019-02-26 11:13:19 +0800

    brand

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,8 +1,9 @@
 <nav class="navbar navbar-dark bg-dark fixed-top navbar-expand-md">
     <div class="container">
         <!-- Branding Image -->
-        {{ link_to_route('home', config('app.name', 'Laravel'), [], ['class' => 'navbar-brand']) }}
-        <img src="{{ asset('FTs1.png')}}" alt="">
+
+        <a href="{{route('home')}}" class="navbar-left"><img src="{{ asset('FTs1.png')}}" width="100%" alt=""></a>
+
         <!-- Collapsed Hamburger -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
